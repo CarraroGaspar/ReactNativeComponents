1cb33a78951c26a5cdd725b2998eae56c0b6e8a6
Gaspar Carraro <gaspar.carraro@gmail.com>
2021-04-14 19:55:37 -0300

    Componente card terminado, tiene un bug el costado derecho de la targeta que hace que los componentes no se vean a la derecha

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { BoxShadow } from './components/BoxShadow/BoxShadow'
+import CardHeader from './components/Card/CardHeader';
+import Card from './components/Card/Card'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{display:"flex", justifyContent: "center", flexDirection: "column", flex: 1}}>
+      <Card 
+        leftHeader={<TouchableOpacity style={{backgroundColor: "yellow"}}><Image
+          style={{
+            width: 50,
+            height: 50,
+          }}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        /></TouchableOpacity>} 
+        title={<Text style={{fontWeight: "bold", fontSize: 16}}>React Native</Text>} 
+        subtitle={<Text style={{fontSize: 12, color: "#AEAEAE"}}>Prueba</Text>} 
+        rightHeader={<TouchableOpacity style={{width: 50, height: 50, backgroundColor: "#EAEAEA", padding: 3, borderRadius: 6, justifyContent: "center", alignItems: "center"}}><Text>OK</Text></TouchableOpacity>}
+      />
+      <Card 
+        leftHeader={<Image
+          style={{
+            width: 50,
+            height: 50,
+          }}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />} 
+        title={<Text style={{fontWeight: "bold", fontSize: 16}}>React Native 2</Text>} 
+        subtitle={<Text style={{fontSize: 12, color: "#AEAEAE"}}>Prueba 2</Text>} 
+        rightHeader={<TouchableOpacity style={{width: 50, height: 50, backgroundColor: "#EAEAEA", padding: 3, borderRadius: 6, justifyContent: "center", alignItems: "center"}}><Text>OK</Text></TouchableOpacity>}
+      />
     </View>
   );
 }
