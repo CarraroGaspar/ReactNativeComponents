2dc2065ec2c2b5d7d8d87416d081b9792a9418e0
Gaspar Carraro <gaspar.carraro@gmail.com>
2021-05-24 17:27:14 -0300

    Borro archivos innesesarios

--- components/Font/Title.js
+++
@@ -1,6 +0,0 @@
-import { Text } from 'react-native'
-import { Styles } from '../../styles/Style'
-
-function Title() {
-    return <Text style={[Styles.fonts.font0, Styles.colours.black]}></Text>
-}
