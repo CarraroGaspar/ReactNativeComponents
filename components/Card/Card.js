1bd412336becf402819dced4b53d23a8e7fa2e40
claudioallende <allendeclaudio22@gmail.com>
2021-05-25 14:37:43 -0300

    corregimos referencia a los estilos

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -5,12 +5,12 @@ import { BoxShadow } from '../BoxShadow/BoxShadow'
 import CardHeader  from './CardHeader'
 //import CardBody  from './CardBody'
 import CardFooter  from './CardFooter'
-import { CardStyle } from '../../styles/Style'
+import { Style } from '../../styles/Style'
 import CardBody from './CardBody';
 
 export default function Card({leftHeader, title, subtitle, rightHeader, topBody, bottomBody, leftFooter, middleFooter, rightFooter }) {
     return <BoxShadow>
-        <View style={CardStyle.card}>
+        <View style={Style.card}>
             <CardHeader left={leftHeader} title={title} subtitle={subtitle} right={rightHeader}/>
             <CardBody top={topBody} bottom={bottomBody}></CardBody>
             <CardFooter left={leftFooter} middle={middleFooter} right={rightFooter}></CardFooter>
